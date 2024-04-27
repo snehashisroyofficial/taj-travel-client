d1273496ffff32f53f79385311d70d03c8fcc4c9
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-04-27 13:52:51 +0530

    dot env successfully implement

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBV65gUzifk55ap94tJYgt4r0LnreK8YTo",
-  authDomain: "assignment-10-e8407.firebaseapp.com",
-  projectId: "assignment-10-e8407",
-  storageBucket: "assignment-10-e8407.appspot.com",
-  messagingSenderId: "81996304440",
-  appId: "1:81996304440:web:17c7c5da1f6715b2d456cd",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
